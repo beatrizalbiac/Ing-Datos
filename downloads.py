ac4c2869c67d58644111c195a74b80010becd242
Beatriz Albiac <alu.161043@usj.es>
2025-09-22 18:27:04 +0200

    changes

---
+++ downloads.py
@@ -0,0 +1,50 @@
+import os
+import requests
+from zipfile import ZipFile
+import pandas as pd
+
+url=[
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip" # it's wrong it should be 2020
+]
+
+path = "C:\\Users\\beaad\\Desktop\\3º\\data engineering\\repos\\Ing-Datos\\downloads"
+if not os.path.exists(path):
+    os.makedirs(path)
+    print('folder created')
+
+for i in url:
+    response = requests.get(i)
+    filename = os.path.basename(i)
+    fpath = os.path.join(path, filename)
+    
+
+    if response.status_code == 200:
+        with open(fpath, 'wb') as file:
+            file.write(response.content)
+        print('File downloaded successfully')
+
+        try:
+            with ZipFile(fpath, 'r') as zip_ref:
+                zip_ref.extractall(path)
+                csv_files = [name for name in zip_ref.namelist() if name.endswith(".csv")]
+            print(f'{filename} extracted successfully')
+        except Exception as e:
+            print(f'Failed to extract {filename}: {e}')
+    
+        os.remove(fpath)
+        print('.zip deleted')
+
+    else:
+        print('Failed to download file')
+    
+for i in csv_files:
+    csv_path = os.path.join(path, i)
+    df = pd.read_csv(csv_path, encoding="latin1")
+    print(df.head())
+    print('\n')
