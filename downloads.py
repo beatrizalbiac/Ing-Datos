ba59e726582172e30512cfbcfa9201abcb2e80e9
Beatriz Albiac <alu.161043@usj.es>
2025-10-01 19:20:27 +0200

    comit

--- downloads.py
+++ downloads.py
@@ -46,15 +46,34 @@ dfs = []
 #         print('Failed to download file')
 
 # print("\n")
+dataframes = {}
 
 dfs = [f for f in os.listdir(path) if f.endswith(".csv")] #borrar when not testing
 for i in dfs:
-        csv_path = os.path.join(path, i)
-        df = pd.read_csv(csv_path, encoding="latin1")
-        print(f"{i}:")
-        print(df.head())
-        print("\n")
-        print(f"nulls:{df.isnull().sum()}")
-        print("\n")
+    csv_path = os.path.join(path, i)
+    df = pd.read_csv(csv_path, encoding="latin1")
+    # print(f"{i}:\n{df.isnull().sum()}\n")
+    
+    # store in dictionary with filename as key
+    dataframes[i] = df
+
+print(dataframes.keys())
+print('\n')
+print(dataframes['Divvy_Trips_2018_Q4.csv'].columns)
+print('\n')
+print(dataframes['Divvy_Trips_2020_Q1.csv'].columns)
+print('\n')
+print(dataframes['Divvy_Trips_2019_Q2.csv'].columns)
+
+for i in dataframes:
+    if dataframes[i].columns =! dataframes['Divvy_Trips_2018_Q4.csv'].columns:
+
+
 # 2020-Q1 -> has different columns
-# 2019-Q2 -> doesn't have any correlations with the others (it's unusable)
+# 2019-Q2 -> has different columns
+# Divvy_Trips_2018_Q4.csv 
+# Divvy_Trips_2019_Q1.csv 
+# Divvy_Trips_2019_Q2.csv 
+# Divvy_Trips_2019_Q3.csv 
+# Divvy_Trips_2019_Q4.csv 
+# Divvy_Trips_2020_Q1.csv
