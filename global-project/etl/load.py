27a9e8ad991935598f41a421eb430b453243dc21
beatrizalbiac <alu.161043@usj.es>
2026-01-10 17:28:06 +0100

    created pokemon dim

--- global-project/etl/load.py
+++ global-project/etl/load.py
@@ -4,6 +4,7 @@ from load.generations import load_generations
 from load.types_dim import load_types
 from load.moves import load_moves
 from load.abilities import load_abilities
+from load.pokemons import load_pokemons
 
 
 conn = sqlite3.connect("db/data/dwh.db")
@@ -13,5 +14,6 @@ try:
     load_types(conn)
     load_moves(conn)
     load_abilities(conn)
+    load_pokemons(conn)
 finally:
     conn.close()
---
+++ global-project/etl/load/pokemons.py
@@ -0,0 +1,17 @@
+import sqlite3
+import pandas as pd
+
+def load_pokemons(conn: sqlite3.Connection):
+    cur = conn.cursor()
+    df_pokemon = pd.read_csv("db/data/processed/pokemon_clean.csv")
+
+    cols = ["Name","Tier","HP","Attack","Defense","Special Attack","Special Defense","Speed","total_stats"]
+    rows = list(df_pokemon[cols].itertuples(index=False, name=None))
+
+    cur.executemany(
+        "insert into dim_pokemon(name, tier, hp, attack, defense, special_attack, special_defense, speed, total_stats) values (?, ?, ?, ?, ?, ?, ?, ?, ?)" \
+    " on conflict(name) do nothing", rows)
+
+    conn.commit()
+
+    # the evolutions will be handled later as they need to be updated upon already existing data
